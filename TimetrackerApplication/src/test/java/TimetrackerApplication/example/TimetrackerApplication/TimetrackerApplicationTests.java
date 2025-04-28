b1795807b3d94519197921d0b7c2930990b0c2ca
Azadan <azadan@live.com>
2025-04-28 21:01:45 +0200

    first commit

---
+++ TimetrackerApplication/src/test/java/TimetrackerApplication/example/TimetrackerApplication/TimetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package TimetrackerApplication.example.TimetrackerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

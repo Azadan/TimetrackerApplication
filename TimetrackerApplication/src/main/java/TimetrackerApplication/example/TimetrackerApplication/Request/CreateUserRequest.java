e6d4f5358ed34edef8912b849de605913832d04e
Azadan <azadan@live.com>
2025-04-30 19:02:50 +0200

    put attributes and validation rules in createUserReq

--- TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Request/CreateUserRequest.java
+++ TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Request/CreateUserRequest.java
@@ -1,4 +1,15 @@
 package TimetrackerApplication.example.TimetrackerApplication.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class CreateUserRequest {
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Size(min = 8, max = 100, message = "Password must be at least 8 characters long")
+    @NotBlank(message = "Password is required")
+    private String password;
 }

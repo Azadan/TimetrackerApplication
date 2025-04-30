1e8d3ea4ad33a00070d123f4f3afabc495edd57d
Azadan <azadan@live.com>
2025-04-30 15:14:29 +0200

    userModel

--- TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Model/User.java
+++ TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Model/User.java
@@ -1,4 +1,27 @@
 package TimetrackerApplication.example.TimetrackerApplication.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String isAdmin;
+
+//    List<TimeEntry> timeEntries = new ArrayList<>();
 }

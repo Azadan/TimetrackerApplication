f8a87d81a173ee7cee43cd658dd3e055a21b12a9
Azadan <azadan@live.com>
2025-04-30 19:46:00 +0200

    create convertToDto method

--- TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Service/UserService.java
+++ TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Service/UserService.java
@@ -1,13 +1,17 @@
 package TimetrackerApplication.example.TimetrackerApplication.Service;
 
+import TimetrackerApplication.example.TimetrackerApplication.DTO.UserDto;
 import TimetrackerApplication.example.TimetrackerApplication.Exceptions.UserAlreadyExistException;
+import TimetrackerApplication.example.TimetrackerApplication.Model.Category;
 import TimetrackerApplication.example.TimetrackerApplication.Model.User;
 import TimetrackerApplication.example.TimetrackerApplication.Repository.UserRepository;
 import TimetrackerApplication.example.TimetrackerApplication.Request.CreateUserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +27,18 @@ public class UserService {
                     user.setPassword(createUserRequest1.getPassword());
                     return userRepository.save(user);
                 }).orElseThrow(() -> new UserAlreadyExistException("User with this email already exists"));
+    }
+
+    public UserDto convertToDto(User user) {
+        List<Long> categoryId = user.getCategories().stream()
+                .map(Category::getCategoryId)
+                .toList();
+
+    return new UserDto(
+            user.getUserId(),
+            user.getEmail(),
+            user.isAdmin(),
+            categoryId
+    );
     }
 }

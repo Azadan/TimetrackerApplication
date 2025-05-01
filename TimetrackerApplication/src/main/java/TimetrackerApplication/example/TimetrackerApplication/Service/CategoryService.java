980915977b43880014533dc7ad8083d25695caac
Azadan <azadan@live.com>
2025-05-01 16:47:15 +0200

    convertodto

--- TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Service/CategoryService.java
+++ TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Service/CategoryService.java
@@ -1,5 +1,6 @@
 package TimetrackerApplication.example.TimetrackerApplication.Service;
 
+import TimetrackerApplication.example.TimetrackerApplication.DTO.CategoryDto;
 import TimetrackerApplication.example.TimetrackerApplication.Exceptions.CategoryAlreadyExistException;
 import TimetrackerApplication.example.TimetrackerApplication.Model.Category;
 import TimetrackerApplication.example.TimetrackerApplication.Model.User;
@@ -28,5 +29,15 @@ public class CategoryService {
                     category.setUser(user);
                     return categoryRepository.save(category);
                 }).orElseThrow(() -> new CategoryAlreadyExistException("Category already exists"));
+    }
+
+    public CategoryDto convertToDto(Category category) {
+        return Optional.ofNullable(category)
+                .map(cat -> new CategoryDto(
+                        cat.getCategoryName(),
+                        cat.getDescription(),
+                        cat.getUser().getUserId(),
+                        cat.getCategoryId()
+                )).orElse(null);
     }
 }

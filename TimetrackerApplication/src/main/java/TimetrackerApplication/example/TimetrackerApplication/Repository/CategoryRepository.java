ce0f228d483c56a17b523a87823d612ebc0ed92d
Azadan <azadan@live.com>
2025-05-01 16:09:16 +0200

    Create category repository

---
+++ TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package TimetrackerApplication.example.TimetrackerApplication.Repository;
+
+import TimetrackerApplication.example.TimetrackerApplication.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}

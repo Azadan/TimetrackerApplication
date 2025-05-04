6407391e76f9c16882301ae47979b9fb333a7dca
Azadan <azadan@live.com>
2025-05-04 13:28:24 +0200

    crepository

--- TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Repository/CategoryRepository.java
+++ TimetrackerApplication/src/main/java/TimetrackerApplication/example/TimetrackerApplication/Repository/CategoryRepository.java
@@ -3,7 +3,11 @@ package TimetrackerApplication.example.TimetrackerApplication.Repository;
 import TimetrackerApplication.example.TimetrackerApplication.Model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByCategoryNameAndUser_UserId(String categoryName, Long userId);
+
+    Category findByUser_UserId(Long userId);
 }
